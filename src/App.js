f997e5fe76de2629101249662c79b3725677a4ed
Carolina Galván <carolina.galvanv@gmail.com>
2019-09-24 19:59:08 -0500

    Started front-end development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import microphone from './microphone.png';
+import send from './send.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <header class="header-bar">
+        <div class="container-fluid">
+          <h1 class="display-4">Let's Talk!</h1>
+          <p class="lead">Translator API Explorer</p>
+        </div>
       </header>
+
+      <section class="message-section">
+        <div class="h-75 container-fluid message-output">
+          hola
+          <p>hola</p>
+        </div>
+
+        <div class="container-fluid">
+          <div class="text-bar">
+            <div class="row">
+              <div class="col-1">
+                <button type="button" id="microphone-btn">
+                  <img src={microphone} class="text-button"/>
+                </button>
+              </div>
+              <div class="col-10 text-left">
+                <input type="text" id="text-field" placeholder="Type something here..."/>
+              </div>
+              <div class="col-1">
+                <button type="button" id="send-btn">
+                  <img src={send} class="text-button"/>
+                </button>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+      </section>
+
+      
     </div>
   );
 }
