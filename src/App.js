ae86b16ab2670b4f80c3bf424276847afb1abd3c
Carolina Galván <carolina.galvanv@gmail.com>
2019-09-28 16:08:46 -0500

    Added media querys, fixed code climate issues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        
+
       <header class="header-bar">
         <div class="container-fluid">
           <h1 class="display-4">Let's Talk!</h1>
@@ -17,33 +17,23 @@ function App() {
       </header>
 
       <section class="message-section">
-        <div class="h-75 container-fluid message-output">
-          
-        </div>
+
+        <div class="h-75 container-fluid message-output"></div>
 
         <div class="container-fluid">
-          <div class="text-bar">
-            <div class="row">
-              <div class="col-1">
+          <div class="text-bar text-left">
                 <button type="button" id="microphone-btn">
                   <img src={microphone} alt="responsive-button"/>
                 </button>
-              </div>
-              <div class="col-10 text-left">
                 <input type="text" id="text-field" placeholder="Type something here..."/>
-              </div>
-              <div class="col-1">
-                <button type="button" id="send-btn">
+                <button type="button" id="send-btn" class="align-right">
                   <img src={send} alt="responsive-button"/>
                 </button>
-              </div>
-            </div>
-           
           </div>
         </div>
+
       </section>
 
-      
     </div>
   );
 }
