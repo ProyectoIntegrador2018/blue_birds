26cb447280a43946afd31f869a2e511d11b1e3f3
Jose Andres S <joseandresalazar@gmail.com>
2019-09-29 16:40:33 -0500

    Adds env file to hide Azure's key

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import TestingSDKComponent from "./testingSDKComponent";
-import './App.css';
-import microphone from './assets/microphone.png';
-import send from './assets/send.png';
+import "./App.css";
+import microphone from "./assets/microphone.png";
+import send from "./assets/send.png";
 
 function App() {
   return (
     <div className="App">
-
       <header class="header-bar">
         <div class="container-fluid">
           <h1 class="display-4">Let's Talk!</h1>
@@ -17,23 +16,25 @@ function App() {
       </header>
 
       <section class="message-section">
-
         <div class="h-75 container-fluid message-output"></div>
 
         <div class="container-fluid">
           <div class="text-bar text-left">
-                <button type="button" id="microphone-btn">
-                  <img src={microphone} alt="responsive-button"/>
-                </button>
-                <input type="text" id="text-field" placeholder="Type something here..."/>
-                <button type="button" id="send-btn" class="align-right">
-                  <img src={send} alt="responsive-button"/>
-                </button>
+            <button type="button" id="microphone-btn">
+              <img src={microphone} alt="responsive-button" />
+            </button>
+            <input
+              type="text"
+              id="text-field"
+              placeholder="Type something here..."
+            />
+            <button type="button" id="send-btn" class="align-right">
+              <img src={send} alt="responsive-button" />
+            </button>
           </div>
         </div>
-
       </section>
-
+      <TestingSDKComponent />
     </div>
   );
 }
