432a7ba8d52dbe9abf2c094cde1f4b3609a57c33
CTorres <37394690+CandyTorresF@users.noreply.github.com>
2019-10-09 13:21:53 -0500

    Revert "Started front-end development (#11)"
    This reverts commit fb29fe661940190fc4517e069d0072c779ec24da.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import microphone from './assets/microphone.png';
-import send from './assets/send.png';
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <header class="header-bar">
-        <div class="container-fluid">
-          <h1 class="display-4">Let's Talk!</h1>
-          <p class="lead">Translator API Explorer</p>
-        </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-      <section class="message-section">
-
-        <div class="h-75 container-fluid message-output"></div>
-
-        <div class="container-fluid">
-          <div class="text-bar text-left">
-                <button type="button" id="microphone-btn">
-                  <img src={microphone} alt="responsive-button"/>
-                </button>
-                <input type="text" id="text-field" placeholder="Type something here..."/>
-                <button type="button" id="send-btn" class="align-right">
-                  <img src={send} alt="responsive-button"/>
-                </button>
-          </div>
-        </div>
-
-      </section>
-
     </div>
   );
 }
