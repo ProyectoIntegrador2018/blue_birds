5bba88fb8bdd98a010e39d0c9a265db7e212bc8d
José Andrés Salazar <joseandresalazar@gmail.com>
2019-10-13 19:26:22 -0500

    Adds functionality to edit text returned from STT function (#27)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./App.css";
+import AzureTTSHandler from "./AzureTTSHandler";
 
 import MessageSection from "./components/MessageSection";
+import MessageBar from "./components/MessageBar";
 
 class App extends React.Component {
+  componentDidMount() {
+    AzureTTSHandler.initializeVoiceRecognition(
+      "warning",
+      "phraseInput",
+      "startRecognizeButton"
+    );
+  }
   render() {
     return (
       <div className="App">
-        <header class="header-bar">
-          <div class="container-fluid">
-            <h1 class="display-4">Let's Talk!</h1>
-            <p class="lead">Translator API Explorer</p>
-          </div>
-        </header>
-        <MessageSection />
+        <div id="content-wrap">
+          <header className="header-bar">
+            <div className="container-fluid">
+              <h1 className="display-4">Let's Talk!</h1>
+              <p className="lead">Translator API Explorer</p>
+            </div>
+          </header>
+          <MessageSection />
+        </div>
+        <MessageBar />
       </div>
     );
   }
--- src/components/MessageBar.js
+++ src/components/MessageBar.js
@@ -4,17 +4,25 @@ import Microphone from "./Microphone";
 export default class MessageBar extends React.Component {
   render() {
     return (
-      <div>
-        <div class="text-bar text-left">
-          <div class="row justify-content-center">
-            <div class="col-sm-1">
-              <Microphone />
-            </div>
-            <div class="col-sm-10">
-              <div id="phraseDiv" placeholder="Type something here..." />
-            </div>
-            <div class="col-sm-1">
-              <SendButton />
+      <div
+        className="container-fluid text-bar text-left"
+        style={{ paddingLeft: 0, paddingRight: 0 }}
+      >
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <Microphone />
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                id="phraseInput"
+              />
+              <div className="input-group-append" id="button-addon4">
+                <SendButton />
+              </div>
             </div>
           </div>
         </div>
