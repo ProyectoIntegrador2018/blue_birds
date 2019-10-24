05dcc921a7cc3b98dc44750558f76130d4aac6ab
Jorge Avalos <jorgeavalosaaa@gmail.com>
2019-10-23 19:29:53 -0500

    Código simplificado

--- src/components/Message.js
+++ src/components/Message.js
@@ -6,27 +6,24 @@ class Message extends React.Component {
     super(props);
     this.is_receiver = props.is_receiver;
     this.message = props.message;
+    this.clase = ''
   }
 
   render() {
-    if (this.is_receiver) {
-      return (
-        <div className="right messages">
-          <div class="message">
-            <p>{this.message}</p>
-          </div>
-          
-        </div>
-      );
-    } else {
-      return (
-        <div className="left messages">
-          <div class="message">
-            <p>{this.message}</p>
-          </div> 
-        </div>
-      );
-    }
+    
+     if(this.is_receiver) {
+       this.clase = 'right messages';
+     } else {
+       this.clase = 'left messages';
+     }
+
+     return (
+       <div class={this.clase}>
+        <div class="message">
+             <p>{this.message}</p>
+         </div>
+       </div>
+    );
   }
 }
 
