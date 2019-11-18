f39ef1758622acd88cd0cc4b77763aeb46988f2f
Jorge Avalos <jorgeavalosaaa@gmail.com>
2019-11-18 16:45:56 -0600

    Se simplifica llamada de ReactHtmlParser

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import ReactHtmlParser from 'react-html-parser';
 
 class Message extends React.Component {
   constructor(props) {
